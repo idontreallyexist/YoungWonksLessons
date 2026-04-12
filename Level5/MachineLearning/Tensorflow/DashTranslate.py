30b48347478353bbc5133ead62f424ce41688ba6
idontreallyexist <kevinalexyu09@gmail.com>
2026-04-11 20:40:16 -0700

    Started translation model lesson

---
+++ Level5/MachineLearning/Tensorflow/DashTranslate.py
@@ -0,0 +1,11 @@
+from dash import Dash, html
+
+import tensorflow
+
+app = Dash()
+
+# Requires Dash 2.17.0 or later
+app.layout = [html.Div(children='Hello World')]
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Level5/MachineLearning/Tensorflow/TranslateStreamlit.py
@@ -0,0 +1,228 @@
+import numpy as np
+
+import typing
+from typing import Any, Tuple
+
+import einops
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+import tensorflow as tf
+import tensorflow_text as tf_text
+
+import streamlit as st
+from PIL import Image, ImageOps, ImageDraw, ImageFont
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from joblib import dump, load
+
+class ShapeChecker():
+  def __init__(self):
+    # Keep a cache of every axis-name seen
+    self.shapes = {}
+
+  def __call__(self, tensor, names, broadcast=False):
+    if not tf.executing_eagerly():
+      return
+
+    parsed = einops.parse_shape(tensor, names)
+
+    for name, new_dim in parsed.items():
+      old_dim = self.shapes.get(name, None)
+
+      if (broadcast and new_dim == 1):
+        continue
+
+      if old_dim is None:
+        # If the axis name is new, add its length to the cache.
+        self.shapes[name] = new_dim
+        continue
+
+      if new_dim != old_dim:
+        raise ValueError(f"Shape mismatch for dimension: '{name}'\n"
+                         f"    found: {new_dim}\n"
+                         f"    expected: {old_dim}\n")
+
+class Encoder(tf.keras.layers.Layer):
+  def __init__(self, text_processor, units):
+    super(Encoder, self).__init__()
+    self.text_processor = text_processor
+    self.vocab_size = text_processor.vocabulary_size()
+    self.units = units
+
+    # The embedding layer converts tokens to vectors
+    self.embedding = tf.keras.layers.Embedding(self.vocab_size, units,
+                                               mask_zero=True)
+
+    # The RNN layer processes those vectors sequentially.
+    self.rnn = tf.keras.layers.Bidirectional(
+        merge_mode='sum',
+        layer=tf.keras.layers.GRU(units,
+                            # Return the sequence and state
+                            return_sequences=True,
+                            recurrent_initializer='glorot_uniform'))
+
+  def call(self, x):
+    shape_checker = ShapeChecker()
+    shape_checker(x, 'batch s')
+
+    # 2. The embedding layer looks up the embedding vector for each token.
+    x = self.embedding(x)
+    shape_checker(x, 'batch s units')
+
+    # 3. The GRU processes the sequence of embeddings.
+    x = self.rnn(x)
+    shape_checker(x, 'batch s units')
+
+    # 4. Returns the new sequence of embeddings.
+    return x
+
+  def convert_input(self, texts):
+    texts = tf.convert_to_tensor(texts)
+    if len(texts.shape) == 0:
+      texts = tf.convert_to_tensor(texts)[tf.newaxis]
+    context = self.text_processor(texts).to_tensor()
+    context = self(context)
+    return context
+  
+class CrossAttention(tf.keras.layers.Layer):
+  def __init__(self, units, **kwargs):
+    super().__init__()
+    self.mha = tf.keras.layers.MultiHeadAttention(key_dim=units, num_heads=1, **kwargs)
+    self.layernorm = tf.keras.layers.LayerNormalization()
+    self.add = tf.keras.layers.Add()
+
+  def call(self, x, context):
+    shape_checker = ShapeChecker()
+
+    shape_checker(x, 'batch t units')
+    shape_checker(context, 'batch s units')
+
+    attn_output, attn_scores = self.mha(
+        query=x,
+        value=context,
+        return_attention_scores=True)
+
+    shape_checker(x, 'batch t units')
+    shape_checker(attn_scores, 'batch heads t s')
+
+    # Cache the attention scores for plotting later.
+    attn_scores = tf.reduce_mean(attn_scores, axis=1)
+    shape_checker(attn_scores, 'batch t s')
+    self.last_attention_weights = attn_scores
+
+    x = self.add([x, attn_output])
+    x = self.layernorm(x)
+
+    return x
+  
+class Decoder(tf.keras.layers.Layer):
+  @classmethod
+  def add_method(cls, fun):
+    setattr(cls, fun.__name__, fun)
+    return fun
+
+  def __init__(self, text_processor, units):
+    super(Decoder, self).__init__()
+    self.text_processor = text_processor
+    self.vocab_size = text_processor.vocabulary_size()
+    self.word_to_id = tf.keras.layers.StringLookup(
+        vocabulary=text_processor.get_vocabulary(),
+        mask_token='', oov_token='[UNK]')
+    self.id_to_word = tf.keras.layers.StringLookup(
+        vocabulary=text_processor.get_vocabulary(),
+        mask_token='', oov_token='[UNK]',
+        invert=True)
+    self.start_token = self.word_to_id('[START]')
+    print(self.start_token)
+    self.end_token = self.word_to_id('[END]')
+
+    self.units = units
+
+
+    # 1. The embedding layer converts token IDs to vectors
+    self.embedding = tf.keras.layers.Embedding(self.vocab_size,
+                                               units, mask_zero=True)
+
+    # 2. The RNN keeps track of what's been generated so far.
+    self.rnn = tf.keras.layers.GRU(units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+
+    # 3. The RNN output will be the query for the attention layer.
+    self.attention = CrossAttention(units)
+
+    # 4. This fully connected layer produces the logits for each
+    # output token.
+    self.output_layer = tf.keras.layers.Dense(self.vocab_size)
+
+class Translator(tf.keras.Model):
+  @classmethod
+  def add_method(cls, fun):
+    setattr(cls, fun.__name__, fun)
+    return fun
+
+  def __init__(self, units,
+               context_text_processor,
+               target_text_processor):
+    super().__init__()
+    # Build the encoder and decoder
+    encoder = Encoder(context_text_processor, units)
+    decoder = Decoder(target_text_processor, units)
+
+    self.encoder = encoder
+    self.decoder = decoder
+
+  def call(self, inputs):
+    context, x = inputs
+    context = self.encoder(context)
+    logits = self.decoder(context, x)
+
+    #TODO(b/250038731): remove this
+    try:
+      # Delete the keras mask, so keras doesn't scale the loss+accuracy.
+      del logits._keras_mask
+    except AttributeError:
+      pass
+
+    return logits
+  
+  def translate(self,
+              texts, *,
+              max_length=50,
+              temperature=0.0):
+    # Process the input texts
+    context = self.encoder.convert_input(texts)
+    batch_size = tf.shape(texts)[0]
+
+    # Setup the loop inputs
+    tokens = []
+    attention_weights = []
+    next_token, done, state = self.decoder.get_initial_state(context)
+
+    for _ in range(max_length):
+        # Generate the next token
+        next_token, done, state = self.decoder.get_next_token(
+            context, next_token, done,  state, temperature)
+
+        # Collect the generated tokens
+        tokens.append(next_token)
+        attention_weights.append(self.decoder.last_attention_weights)
+
+        if tf.executing_eagerly() and tf.reduce_all(done):
+            break
+
+    # Stack the lists of tokens and attention weights.
+    tokens = tf.concat(tokens, axis=-1)   # t*[(batch 1)] -> (batch, t)
+    self.last_attention_weights = tf.concat(attention_weights, axis=1)  # t*[(batch 1 s)] -> (batch, t s)
+
+    result = self.decoder.tokens_to_text(tokens)
+    return result
+
+reloaded = tf.saved_model.load('translator')
+
+text_prompt=st.text_input("Text to Translate")
+if st.button("Translate"):
+  result = reloaded.translate(tf.constant([text_prompt]))
+  st.write(result)
